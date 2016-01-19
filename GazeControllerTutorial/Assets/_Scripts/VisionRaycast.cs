0f7b3fa02b6e2c07dab861842a346c455a2476ba
Rosstin Murphy <asterazul@gmail.com>
2016-01-18 16:20:04 -0800

    002- VisionRaycast detects whether or not the player gaze is hitting an object.

---
+++ GazeControllerTutorial/Assets/_Scripts/VisionRaycast.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisionRaycast : MonoBehaviour {
+
+    public Camera playerCamera; // holds a reference to the player camera
+
+	void Start () {
+	    
+	}
+	
+	void Update () {
+        RaycastHit hit = new RaycastHit(); // this object will collect data about the collision each frame
+
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit)) // do the raycast, based on the camera's position and orientation, and store the hit for our reference
+        {
+            Debug.Log("We are hitting something!"); // the raycast collided with an object
+        }
+        else // there wasn't any collision
+        {
+            Debug.Log("Not hitting anything.");
+        }
+    }
+}
