8cb85b47d42e1f3b78818151b82db26221851c08
Rosstin Murphy <asterazul@gmail.com>
2016-01-18 16:34:51 -0800

    003- the cursor flies at the screen like crazy!

--- GazeControllerTutorial/Assets/_Scripts/VisionRaycast.cs
+++ GazeControllerTutorial/Assets/_Scripts/VisionRaycast.cs
@@ -5,20 +5,22 @@ public class VisionRaycast : MonoBehaviour {
 
     public Camera playerCamera; // holds a reference to the player camera
 
+    public GameObject cursor; // holds a reference to a cursor object to be drawn where the player's gaze points
+
 	void Start () {
-	    
-	}
-	
-	void Update () {
+    }
+
+    void Update () {
         RaycastHit hit = new RaycastHit(); // this object will collect data about the collision each frame
 
         if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit)) // do the raycast, based on the camera's position and orientation, and store the hit for our reference
         {
-            Debug.Log("We are hitting something!"); // the raycast collided with an object
+            cursor.SetActive(true); // activate the cursor if it was inactive
+            cursor.transform.position = hit.point; 
         }
         else // there wasn't any collision
         {
-            Debug.Log("Not hitting anything.");
+            cursor.SetActive(false); // deactivate the cursor if it was active
         }
     }
 }
